0c2a7e9d8c486fff8d30aea5f10cc1b6770ffd3d
Propriétaire <jayetlucas@gmail.com>
2021-09-01 13:48:04 +0200

    Create AuthController + allow cors origin

--- src/main/java/com/upet/animalerie/Controller/AuthController.java
+++ src/main/java/com/upet/animalerie/Controller/AuthController.java
@@ -1,4 +1,24 @@
 package com.upet.animalerie.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("Auth")
 public class AuthController {
+
+    /*@Autowired
+    private User user;*/
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("exist")
+    public String userExist(@RequestParam String email) {
+        /*if (user != null){
+            return "il existe";
+        }
+        else {
+            return "il existe pas";
+        }*/
+        return "isExist";
+    }
 }
