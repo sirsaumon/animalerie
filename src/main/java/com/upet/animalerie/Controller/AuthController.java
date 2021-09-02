a971e4b296e01981a0040d2e392844f54430148a
Propriétaire <jayetlucas@gmail.com>
2021-09-02 14:23:34 +0200

    Register + connection

--- src/main/java/com/upet/animalerie/Controller/AuthController.java
+++ src/main/java/com/upet/animalerie/Controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.upet.animalerie.Controller;
 
+import com.upet.animalerie.Entities.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -7,18 +9,31 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("Auth")
 public class AuthController {
-
     /*@Autowired
-    private User user;*/
+    private UserRepository repositorie;
+
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("exist")
     public String userExist(@RequestParam String email) {
+        UserEntity user = repositorie
         /*if (user != null){
             return "il existe";
         }
         else {
             return "il existe pas";
-        }*/
+        }
         return "isExist";
+    }*/
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("register")
+    public String registerNewUser(@RequestParam String nom, @RequestParam String prenom, @RequestParam String type) {
+        if (type == "acheteur") {
+
+        }
+        else if (type == "vendeur"){
+
+        }
+        return "success";
     }
 }
